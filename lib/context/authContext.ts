6fcb96b0bd45ca88b0689e94498c70ff96d2f42c
David Dal Busco <david.dalbusco@outlook.com>
2025-01-25 09:48:22 +0100

    feat: copy Suitec9 react contribution (#2)

---
+++ lib/context/authContext.ts
@@ -0,0 +1,15 @@
+import { User } from "@junobuild/core";
+import { createContext } from "react";
+
+export interface AuthContextType {
+  subscribeCollection: (
+    collectionName: string,
+    callback: (docs: any[]) => void,
+  ) => void;
+  user: User | null | undefined;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  subscribeCollection: () => {},
+  user: null,
+});
